bbe2ac7d049e38120ebae5e338f4df0024ed6ed3
karupanerura <karupa@cpan.org>
2013-09-10 19:31:28 +0900

    for global destruction

--- lib/Yomiuri/Model.pm
+++ lib/Yomiuri/Model.pm
@@ -9,7 +9,13 @@ use String::CamelCase qw/decamelize/;
 
 use Yomiuri::Utils qw/write_file read_file search_project_dir YOMIURI_REPOSITORY_DIR/;
 
-my $json = JSON->new->ascii(1);
+{
+    my $json;
+    sub json {
+        $json ||= JSON->new->ascii(1);
+        return $json;
+    }
+}
 
 sub dir  { File::Spec->catfile(search_project_dir(), YOMIURI_REPOSITORY_DIR()) }
 sub path { File::Spec->catfile($_[0]->dir,           $_[0]->file) }
@@ -44,12 +50,12 @@ sub load {
     return $class->default unless -f $class->path;
 
     my $text = read_file($class->path);
-    return $json->decode($text);
+    return json()->decode($text);
 }
 
 sub save {
     my $self = shift;
-    write_file($self->path, $json->encode(+{ %$self }));
+    write_file($self->path, json()->encode(+{ %$self }));
 }
 
 sub DESTROY {
