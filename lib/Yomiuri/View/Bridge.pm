1b9561e9a1cd6505be4b064dd285d1ce08bbf4f0
karupanerura <karupa@cpan.org>
2013-09-10 20:10:11 +0900

    updated index.tx

--- lib/Yomiuri/View/Bridge.pm
+++ lib/Yomiuri/View/Bridge.pm
@@ -7,6 +7,7 @@ use parent qw/ Text::Xslate::Bridge /;
 
 use Amon2::Declare;
 use Text::Xslate::Util qw/html_builder/;
+use URI;
 
 use Yomiuri::Markdown;
 
@@ -16,10 +17,33 @@ __PACKAGE__->bridge(
     # array    => \%array_methods,
     # hash     => \%hash_methods,
     function => +{
-        config   => sub { c()->config },
-        markdown => html_builder { Yomiuri::Markdown->convert_to_html(@_) },
+        config             => sub { c()->config },
+        markdown           => html_builder { Yomiuri::Markdown->convert_to_html(@_) },
+        markdown_from_file => html_builder { Yomiuri::Markdown->convert_to_html_from_file(@_) },
+        uri_for            => \&_uri_for,
+        cool_uri_for       => \&_cool_uri_for,
     },
 );
+
+sub _uri_for {
+    my ($path, $param) = @_;
+
+    my $uri = URI->new;
+    $uri->path($path);
+    $uri->query_form(%$param, $uri->query_form) if $param;
+
+    return $uri->as_string;
+}
+
+sub _cool_uri_for {
+    my ($path_parts, $param) = @_;
+
+    my $path = '/' . join '/', @$path_parts;
+       $path =~ s{/index$}{/}g;
+
+    return _uri_for($path, $param);
+}
+
 
 1;
 __END__
