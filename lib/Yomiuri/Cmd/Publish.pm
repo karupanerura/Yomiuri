9524757bc70fbda6d3fbe38f775a7c07cf58115b
karupanerura <karupa@cpan.org>
2013-09-11 20:29:49 +0900

    auto add entry when before publish.

--- lib/Yomiuri/Cmd/Publish.pm
+++ lib/Yomiuri/Cmd/Publish.pm
@@ -6,6 +6,7 @@ use utf8;
 use parent qw/Yomiuri::Cmd/;
 
 use File::Spec;
+use File::Find qw/find/;
 use Yomiuri::Utils;
 
 use Yomiuri::Web::C::Root;
@@ -14,7 +15,30 @@ sub run {
     my ($self, $opts) = @_;
 
     my $dir = search_project_dir();
-    write_file(File::Spec->catfile($dir, 'htdocs', 'index.html'), Yomiuri::Web::C::Root->index($self->c));
+    $self->prepare($opts, $dir);
+    $self->render($opts, $dir);
+}
+
+sub prepare {
+    my ($self, $opts, $dir) = @_;
+    return if $opts->{'no-prepare'};
+
+    my $entry = $self->cmd('Entry');
+    find +{
+        no_chdir => 1,
+        wanted   => sub {
+            $entry->add($opts, $_) if -f $_ && $_ =~ /\.(?:md|mkdn)$/;
+        },
+    } => File::Spec->catfile($dir, 'entry');
+}
+
+sub render {
+    my ($self, $opts, $dir) = @_;
+    return if $opts->{'no-render'};
+
+    my $htdocs = File::Spec->catfile($dir, 'htdocs');
+
+    write_file(File::Spec->catfile($htdocs, 'index.html'), Yomiuri::Web::C::Root->index($self->c));
 }
 
 1;
